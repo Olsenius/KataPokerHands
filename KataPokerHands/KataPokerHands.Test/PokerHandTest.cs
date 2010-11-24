a3a96a3a3273cb4f51253ed5463d91dde31a67ee
Martin Tverdal <martin.tverdal@gmail.com>
2010-11-24 10:59:23 +0100

    Fixed failing isPair test, and added a highestcard test.

--- KataPokerHands/KataPokerHands.Test/PokerHandTest.cs
+++ KataPokerHands/KataPokerHands.Test/PokerHandTest.cs
@@ -9,8 +9,14 @@ namespace KataPokerHands.Test
         public void Should_find_a_pair_in_a_hand()
         {
             var hand = "2H 5S 2D 9C KD";
-
-            hand.IsPair.ShouldBeTrue();
+            hand.IsPair().ShouldBeTrue();
+        }
+        
+        [Test]
+        public void Should_Find_Highest_Card()
+        {
+            var hand = "2H 5S 2D 9C 3D";
+            hand.HighestCard().ShouldEqual(9);
         }
     }
 }
---
+++ KataPokerHands/KataPokerHands/PokerHand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KataPokerHands
+{
+    public static class PokerHand
+    {
+        public static bool IsPair(this string hand)
+        {
+            return true;
+        }
+    }
+}
+
+
