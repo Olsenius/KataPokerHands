bb8f549ef59cb1da83df157bff9bfafbe2d1ae8b
Andreas Lien Olsen <andreas.olsen@gmail.com>
2010-11-25 19:42:21 +0100

    Added test method Should_find_highest_card_amongst_non_face_cards

--- KataPokerHands/KataPokerHands.Test/PokerHandTest.cs
+++ KataPokerHands/KataPokerHands.Test/PokerHandTest.cs
@@ -19,7 +19,14 @@ namespace KataPokerHands.Test
             hand.IsPair().ShouldBeFalse();
         }
 
-        [TestCase("9H 9S 9D 9C 9D",9)]
+        [TestCase("2H 2S 2D 2C 2D", 2)]
+        [TestCase("3H 3S 3D 3C 2D", 3)]
+        [TestCase("4H 4S 4D 4C 2D", 4)]
+        [TestCase("5H 5S 5D 5C 2D", 5)]
+        [TestCase("6H 6S 6D 6C 2D", 6)]
+        [TestCase("7H 7S 7D 7C 2D", 7)]
+        [TestCase("8H 8S 8D 8C 2D", 8)]
+        [TestCase("9H 9S 9D 9C 9D", 9)]
         public void Should_find_highest_card_amongst_non_face_cards(string hand, int highest)
         {
             hand.HighestCard().ShouldEqual(highest);
