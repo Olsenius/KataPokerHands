87b0b01fdaced63de0e12c2f346edbc71332a019
Andreas Lien Olsen <andreas.olsen@gmail.com>
2010-11-27 13:54:10 +0100

    Moved extension methods to a new class. Refactored HighestCard into a new methof HighestCard in a new PokerHand class

--- KataPokerHands/KataPokerHands.Test/PokerHandTest.cs
+++ KataPokerHands/KataPokerHands.Test/PokerHandTest.cs
@@ -19,17 +19,18 @@ namespace KataPokerHands.Test
             hand.IsPair().ShouldBeFalse();
         }
 
-        [TestCase("2H 2S 2D 2C 2D", 2)]
-        [TestCase("3H 3S 3D 3C 2D", 3)]
-        [TestCase("4H 4S 4D 4C 2D", 4)]
-        [TestCase("5H 5S 5D 5C 2D", 5)]
-        [TestCase("6H 6S 6D 6C 2D", 6)]
-        [TestCase("7H 7S 7D 7C 2D", 7)]
-        [TestCase("8H 8S 8D 8C 2D", 8)]
-        [TestCase("9H 9S 9D 9C 9D", 9)]
-        public void Should_find_highest_card_amongst_cards_identified_by_numbers(string hand, int highest)
+        [TestCase("2H 2S 2D 2C 2D", "2")]
+        [TestCase("3H 3S 3D 3C 2D", "3")]
+        [TestCase("4H 4S 4D 4C 2D", "4")]
+        [TestCase("5H 5S 5D 5C 2D", "5")]
+        [TestCase("6H 6S 6D 6C 2D", "6")]
+        [TestCase("7H 7S 7D 7C 2D", "7")]
+        [TestCase("8H 8S 8D 8C 2D", "8")]
+        [TestCase("9H 9S 9D 9C 9D", "9")]
+        public void Should_find_highest_card_amongst_cards_identified_by_numbers(string hand, string highest)
         {
-            hand.HighestCard().ShouldEqual(highest.ToString());
+            var pokerHand = new PokerHand(hand);
+            pokerHand.HighestCard().ShouldEqual(highest);
         }
 
         [TestCase("9H 9S 9D 9C TD", "T")]
@@ -39,7 +40,8 @@ namespace KataPokerHands.Test
         [TestCase("5H AS 5D 5C KD", "A")]
         public void Should_find_highest_card_amongst_cards_over_nine(string hand, string highest)
         {
-            hand.HighestCard().ShouldEqual(highest);
+            var pokerHand = new PokerHand(hand);
+            pokerHand.HighestCard().ShouldEqual(highest);
         }
     }
 }
--- KataPokerHands/KataPokerHands/PokerHand.cs
+++ KataPokerHands/KataPokerHands/PokerHand.cs
@@ -1,61 +1,49 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace KataPokerHands
 {
-    public static class PokerHand
+    public class PokerHand
     {
-        static Dictionary<string, int> cards = new Dictionary<string, int>()
+        private readonly string _hand;
+
+        public PokerHand(string hand)
         {
-            {"2", 2},
-            {"3", 3},
-            {"4", 4},
-            {"5", 5},
-            {"6", 6},
-            {"7", 7},
-            {"8", 8},
-            {"9", 9},
-            {"T", 10},
-            {"J", 11},
-            {"Q", 12},
-            {"K", 13},
-            {"A", 14},
-        };
-        public static bool IsPair(this string hand)
+            _hand = hand;
+        }
+
+        public string HighestCard()
         {
-            HashSet<int> s = new HashSet<int>();
-            foreach (string card in hand.Split(' '))
+            var cards = _hand.Split(' ');
+            return GetCardValueAsText(cards.Max(x => GetCardValue(x)));
+        }
+
+        private int GetCardValue(string card)
+        {
+            var value = card[0].ToString();
+
+            return _cardValues.First(kv => kv.Key == value).Value;
+        }
+
+        private static string GetCardValueAsText(int card)
+        {
+            switch (card)
             {
-                if(!s.Add(card[0])){
-                    return true;
-                }
+                case 10:
+                    return "T";
+                case 11:
+                    return "J";
+                case 12:
+                    return "Q";
+                case 13:
+                    return "K";
+                case 14:
+                    return "A";
+                default:
+                    return card.ToString();
             }
-            return false;
         }
 
-        public static int ToPokerValue(this string card) 
-        {
-            return cards[card];
-        }
-
-        public static string ToPokerStringValue(this int card)
-        {
-            foreach (KeyValuePair<string, int> kp in cards)
-            {
-                if (kp.Value == card)
-                {
-                    return kp.Key;
-                }
-            }
-            return "Q";
-        }
-
-        public static string HighestCard(this string hand)
-        {
-            var cards = hand.Split(' ');
-            return cards.Max(card => card[0].ToString().ToPokerValue()).ToPokerStringValue();
-        }
+        private Dictionary<string, int> _cardValues = new Dictionary<string, int>() { { "2", 2 }, { "3", 3 }, { "4", 4 }, { "5", 5 }, { "6", 6 }, { "7", 7 }, { "8", 8 }, { "9", 9 }, { "T", 10 }, { "J", 11 }, { "Q", 12 }, { "K", 13 }, { "A", 14 }, };
     }
 }
-
-
