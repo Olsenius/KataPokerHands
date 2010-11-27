4129910b07f5e1e8e91c3a2e2d40343d853875eb
Andreas Lien Olsen <andreas.olsen@gmail.com>
2010-11-27 13:35:20 +0100

    Renamed PokerHand to PokerHandStringExtensions

---
+++ KataPokerHands/KataPokerHands/PokerHandStringExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KataPokerHands
+{
+    public static class PokerHandStringExtensions
+    {
+        static Dictionary<string, int> cards = new Dictionary<string, int>()
+        {
+            {"2", 2},
+            {"3", 3},
+            {"4", 4},
+            {"5", 5},
+            {"6", 6},
+            {"7", 7},
+            {"8", 8},
+            {"9", 9},
+            {"T", 10},
+            {"J", 11},
+            {"Q", 12},
+            {"K", 13},
+            {"A", 14},
+        };
+        public static bool IsPair(this string hand)
+        {
+            HashSet<int> s = new HashSet<int>();
+            foreach (string card in hand.Split(' '))
+            {
+                if(!s.Add(card[0])){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static int ToPokerValue(this string card) 
+        {
+            return cards[card];
+        }
+
+        public static string ToPokerStringValue(this int card)
+        {
+            foreach (KeyValuePair<string, int> kp in cards)
+            {
+                if (kp.Value == card)
+                {
+                    return kp.Key;
+                }
+            }
+            return "Q";
+        }
+
+        public static string HighestCard(this string hand)
+        {
+            var cards = hand.Split(' ');
+            return cards.Max(card => card[0].ToString().ToPokerValue()).ToPokerStringValue();
+        }
+    }
+}
+
+
